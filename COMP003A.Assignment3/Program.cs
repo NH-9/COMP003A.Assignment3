2b194f7477bdc1ca541db62ee4d5bb124108f639
NH <noahhulett@my.whccd.edu>
2026-01-30 18:25:35 -0800

    age calculation and driving ability determination

--- COMP003A.Assignment3/Program.cs
+++ COMP003A.Assignment3/Program.cs
@@ -12,7 +12,21 @@ namespace COMP003A.Assignment3
             Console.Write("Enter your birth year: ");
             int inputBirth = int.Parse(Console.ReadLine());
 
-
+            int age = DateTime.Now.Year - inputBirth;
+            string ageStatus = "";
+            if (age < 16)
+            {
+                ageStatus = "Not Allowed";
+            }
+            else if (age >= 16 && age < 18)
+            {
+                ageStatus = "Allowed Under Supervision";
+            }
+            else
+            {
+                ageStatus = "Allowed";
+            }
+            Console.WriteLine($"\nCalculated Age: {age}\nAble To Drive: {ageStatus}");
         }
     }
 }
