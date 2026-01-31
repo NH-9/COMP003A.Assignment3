5c745d112a23bc2e77d9e937d4a3848bbf06d04c
NH <noahhulett@my.whccd.edu>
2026-01-30 20:22:20 -0800

    output guidance based on user choice

--- COMP003A.Assignment3/Program.cs
+++ COMP003A.Assignment3/Program.cs
@@ -34,10 +34,11 @@ namespace COMP003A.Assignment3
             Console.Write("Do you have a valid Driver's License? (yes/no): ");
             bool inputLicense = Console.ReadLine() == "yes";
 
+
             string readyStatus = "";
             if (ageStatus == "Allowed Under Supervision")
             {
-                if (inputLicense ||  inputPermit)
+                if (inputLicense || inputPermit)
                 {
                     readyStatus = "Conditionally Ready";
                 }
@@ -84,6 +85,42 @@ namespace COMP003A.Assignment3
             }
             Console.Write("Enter Choice: ");
             string inputGuidance = Console.ReadLine();
+
+            Console.WriteLine("\nGuidance:");
+            switch (inputGuidance)
+            {
+                case "RDL":
+                    Console.WriteLine("Refresh your memory on the rules of the road.");
+                    break;
+
+                case "RAV":
+                    Console.WriteLine("Follow documented steps to register a vehicle at your local DMV.");
+                    break;
+
+                case "PIN":
+                    Console.WriteLine("You must have an insured vehicle to drive; research what insurance plan will work best for you.");
+                    break;
+
+                case "TBT":
+                    Console.WriteLine("Schedule a behind-the-wheel exam at your local DMV to obtain a driver's license.");
+                    break;
+
+                case "PDS":
+                    Console.WriteLine("Find a safe area to practice your driving skills, such as an empty parking lot.");
+                    break;
+
+                case "LDL":
+                    Console.WriteLine("Read and memorize official driving law documenation.");
+                    break;
+
+                case "TPT":
+                    Console.WriteLine("Take the pratice multiple-choice test on the DMV website to test your knowledge");
+                    break;
+
+                case "TLT":
+                    Console.WriteLine("Schedule a multiple-choice exam on driving laws at your local DMV to obtain a learner's permit.");
+                    break;
+            }
         }
     }
 }
